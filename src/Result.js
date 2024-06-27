d63b616e4404b2df857aca413fa3f1d653854f80
Pasiduchamod <jayaweera.pasindu123@gmail.com>
2024-06-28 00:16:36 +0530

    UI build

--- src/Result.js
+++ src/Result.js
@@ -2,9 +2,24 @@ import React from 'react'
 
 const Result = () => {
   return (
-    <div>
-      
-    </div>
+    <ul>
+        <li className='World'>
+            <h2>Hello</h2>
+            <span>/he'leo/ /he'lou/</span>
+        </li>
+        <li className='contain'>
+            <h3>noun</h3>
+
+            <div className='details-meaning'>
+                <h3>Meaning</h3>
+                <p>-"Hello! or an equivalent greeting.</p>
+            </div>
+            <div className='details synonyms'>
+                <h3>Synonyms</h3>
+                <p>- "Hello! or an equivalent greeting.</p>
+            </div>
+        </li>
+    </ul>
   )
 }
 
