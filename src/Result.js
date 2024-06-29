964680ef80ebd20dfc7638d97a12861a25605587
Pasiduchamod <jayaweera.pasindu123@gmail.com>
2024-06-29 10:49:14 +0530

    project completed(styles to be done)

--- src/Result.js
+++ src/Result.js
@@ -1,24 +1,46 @@
 import React from 'react'
 
-const Result = () => {
+const Result = ({word,phonetics,meanings,setText}) => {
   return (
     <ul>
-        <li className='World'>
-            <h2>Hello</h2>
-            <span>/he'leo/ /he'lou/</span>
+        <li className='Word'>
+            <h2>{word}</h2>
+            {
+                phonetics.map((phonetics,index)=>(
+                    <span key={index}>{phonetics.text}</span>
+                ))
+            }
         </li>
-        <li className='contain'>
-            <h3>noun</h3>
 
-            <div className='details-meaning'>
-                <h3>Meaning</h3>
-                <p>-"Hello! or an equivalent greeting.</p>
-            </div>
-            <div className='details synonyms'>
-                <h3>Synonyms</h3>
-                <p>- "Hello! or an equivalent greeting.</p>
-            </div>
+        {
+            meanings.map((meanings, index)=>(
+            <li className='contain'>
+                 <h3>noun</h3>
+
+                <div className='details meaning'>
+                    <h3>Meaning</h3>
+                    {
+                        meanings.definitions.map((definition,index)=>(
+                            <p key={index}>- {definition.definition}</p>
+                        ) )
+                    }
+                 </div>
+
+                 {
+                    meanings.synonyms.length !==0&&
+                    <div className='details synonyms'>
+                 <h3>Synonyms</h3>
+                 {
+                    meanings.synonyms.map((synonym, index)=>(
+                        <span key={index} onClick={()=> setText(synonym)}>{`${synonym}, `},</span>
+                    ))
+                 }
+                </div>
+                 }
+               
         </li>
+            ))
+        }
     </ul>
   )
 }
